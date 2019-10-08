975630960e47e326ae4c5c86110633e8b2199539
Frank <frank.pettersson1@gmail.com>
2019-10-08 16:40:00 +0200

    epik

--- src/bopandrebop.vue
+++ src/bopandrebop.vue
@@ -1,11 +1,12 @@
 <template lang="pug">
   #bopandrebop
+    #logo
+      a bopandrebop
     #nav
       router-link(to="/") HOME
       router-link(to="/records") RECORDS
       router-link(to="/contact") CONTACT
     router-view
-    #footer
 </template>
 
 <style lang="scss">
@@ -15,31 +16,49 @@ body {
   margin: 0;
 }
 #bopandrebop {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  font-family: 'Roboto', sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  background-color: rgb(219, 219, 219);
+  width: 70%;
+  margin: 0 auto;
 }
+#logo {
+  a {
+    border: 2px solid black;
+    padding: 30px 60px;
+    display: inline-block;
+    font-size: 3em;
+    font-weight: bolder;
+    text-decoration: none;
+    color: black;
+    margin: 0.5em 0;
+
+    &:hover {
+      background-color: rgb(240, 240, 240);
+    }
+  }
+}
+
 #nav {
   padding: 30px;
   a {
     font-weight: bold;
     color: #2c3e50;
     padding: 0 8px;
-    border-right: 2px solid black;
-    &:last-child {
-      border-right: none;
-      padding: 0 0 0 8px;
-    }
+    border: 2px solid black;
+    padding: 10px 20px;
+    margin: 0 10px;
+    text-decoration: none;
     &.router-link-exact-active {
-      color: #42b983;
+      background-image: url('https://thumbs.gfycat.com/HelpfulDazzlingBluebottlejellyfish-max-1mb.gif');
+      background-position: top center;
+      background-size: 90px 90px;
+      background-repeat: no-repeat;
+      text-shadow: 1px 1px 5px #000000;
+      color: white;
     }
   }
-}
-#footer {
-  height: 400px;
-  background-color: #777777;
 }
 </style>
--- vue.config.js
+++ vue.config.js
@@ -1,3 +1,15 @@
+const GoogleFontsPlugin = require('google-fonts-webpack-plugin')
+
 module.exports = {
-  productionSourceMap: false
+  productionSourceMap: false,
+  configureWebpack: {
+    plugins: [
+      new GoogleFontsPlugin({
+        fonts: [
+          { family: 'Source Sans Pro' },
+          { family: 'Roboto', variants: ['400', '700'] }
+        ]
+      })
+    ]
+  }
 }
